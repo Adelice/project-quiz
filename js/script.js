c573f11685b31eec1e623bf4948a69b37b7b03cc
Adelice <nancytoniw@gmail.com>
2018-12-21 16:59:46 +0200

    insert javascript

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var testResults = function(one, two , three ,four , five ,){
+    return one + two + three + four + five ;
+    }
+ 
+    $(document).ready(function(){
+      $(".begin").click(function(){
+        $("#form").fadeToggle("slow");
+      });
+ 
+      $("form#test").submit(function(event){
+        var question1 = parseInt($("input:radio[name=question1]:checked").val());
+        var question2 = parseInt($("input:radio[name=question2]:checked").val());
+        var question3 = parseInt($("input:radio[name=question3]:checked").val());
+        var question4 = parseInt($("input:radio[name=question4]:checked").val());
+        var question5= parseInt($("input:radio[name=question5]:checked").val());
+ 
+        var result = testResults(question1, question2 ,question3, question4, question5);
+ 
+        $("#display").text("Your score is:" + result);
+ 
+        event.preventDefault();
+        $(".fl").show();
+      });
+    });
